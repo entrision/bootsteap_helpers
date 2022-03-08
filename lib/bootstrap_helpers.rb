e746f8733ac369af78557552334c58796947a59b
Robert D. Cotey II <rcotey@entrision.com>
2022-03-08 14:00:32 -0500

    Initial setup of a few components

--- lib/bootstrap_helpers.rb
+++ lib/bootstrap_helpers.rb
@@ -1,6 +1,14 @@
-require "bootstrap_helpers/version"
+# frozen_string_literal: true
+
+require 'bootstrap_helpers/version'
+require 'bootstrap_helpers/grid'
+require 'bootstrap_helpers/navbar'
+require 'bootstrap_helpers/railtie' if defined?(Rails)
 
 module BootstrapHelpers
   class Error < StandardError; end
   # Your code goes here...
+
+  class Engine < ::Rails::Engine
+  end
 end
