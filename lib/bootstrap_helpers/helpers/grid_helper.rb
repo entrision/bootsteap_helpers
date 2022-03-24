2a0ec9aec1a2d96615072a4aa5749b45db5b6e1e
Robert D. Cotey II <rcotey@entrision.com>
2022-03-24 15:57:31 -0400

    remove commented code

--- lib/bootstrap_helpers/helpers/grid_helper.rb
+++ lib/bootstrap_helpers/helpers/grid_helper.rb
@@ -21,11 +21,6 @@ module BootstrapHelpers
         id = options[:id] || SecureRandom.uuid
         content = capture(grid, &block)
         bs_render template: 'grid/column', content: content, locals: { classes: classes, id: id, style: options[:style], content: content }
-        # bs_render(template: 'grid/column', locals: { classes: classes, id: id, style: options[:style] }, &block)
-      end
-
-      def grid_render(partial_name, locals: {})
-        render(partial: "bootstrap/grid/#{partial_name}", locals: locals)
       end
     end
   end
