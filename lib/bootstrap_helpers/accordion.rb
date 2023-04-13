5e4a1066caf1a3605542a33eac46782a2c87940c
Robert D. Cotey II <rcotey@entrision.com>
2023-04-13 15:04:25 -0400

    Added option to close accordions

--- lib/bootstrap_helpers/accordion.rb
+++ lib/bootstrap_helpers/accordion.rb
@@ -4,17 +4,19 @@ module BootstrapHelpers
   class Accordion < BootstrapHelpers::Base
     include ActionView::Helpers::CaptureHelper
 
-    attr_reader :parent, :first_one
+    attr_reader :parent, :first_one, :always_collapse
 
-    def initialize(view_context)
+    def initialize(view_context, options = {})
       @ac = view_context
       @parent = "P#{SecureRandom.uuid}"
       @first_one = 0
+      @always_collapse = options[:always_collapse] || false
     end
 
     def item(title: nil, &block)
       @first_one += 1
-      bs_render template: 'accordion/item', locals: { title: title, parent: @parent, first: @first_one < 2 }, block: block
+
+      bs_render template: 'accordion/item', locals: { title: title, parent: @parent, first: (@always_collapse ? false : @first_one < 2) }, block: block
     end
   end
 end
--- lib/bootstrap_helpers/helpers/accordion_helper.rb
+++ lib/bootstrap_helpers/helpers/accordion_helper.rb
@@ -6,7 +6,7 @@ module BootstrapHelpers
       def accordion(options = {}, &block)
         raise ArgumentError, 'Missing block' unless block_given?
 
-        accordion = BootstrapHelpers::Accordion.new(self)
+        accordion = BootstrapHelpers::Accordion.new(self, options)
         bs_render template: 'accordion/accordion', block: block, options: options, klass: accordion
       end
     end
